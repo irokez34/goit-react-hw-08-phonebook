e7d91c1ac51f55f0e7b4e5e912bdf5b0f2e1fefc
irokez34 <124583720+irokez34@users.noreply.github.com>
2024-01-29 15:54:36 +0000

    Deploying to gh-pages from @ irokez34/goit-react-hw-08-phonebook@f37f4f888c65455796c2801cbf294991bb329740 🚀

--- static/js/983.b629a443.chunk.js
+++
@@ -1,2 +0,0 @@
-"use strict";(self.webpackChunkgoit_react_hw_08_phonebook=self.webpackChunkgoit_react_hw_08_phonebook||[]).push([[983],{983:function(e,o,n){n.r(o);var t=n(184);o.default=function(){return(0,t.jsx)("div",{className:"",children:"Home"})}}}]);
-//# sourceMappingURL=983.b629a443.chunk.js.map
